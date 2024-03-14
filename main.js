69aabddf18072fc9a98afb833c9ab93eded3da2b
zhangguoq <zhangguoq@chanjet.com>
2024-03-14 23:32:41 +0800

    科目一

---
+++ main.js
@@ -0,0 +1,15 @@
+var electron = require("electron");
+// 引用app
+var app = electron.app;
+// 窗口引用
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindw = null; // 主窗口
+
+app.on("ready", () => {
+  mainWindw = new BrowserWindow({ width: 800, height: 800 });
+  mainWindw.loadFile("index.html"); //加载页面
+  mainWindw.on("closed", () => {
+    mainWindw = null;
+  });
+});
